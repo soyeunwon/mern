6e1ca778df19aec1aac5d24100f551623f203507
dnjstd <dnjstd@gmail.com>
2024-01-03 23:29:38 +0900

    Feat: Portal Nav

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElement/Backdrop";
 
 const MainNavigation = () => {
+  const [drawrIsOpen, setDrawerIsOpen] = useState(false);
+
+  const drawerHandler = () => {
+    setDrawerIsOpen(!drawrIsOpen);
+  };
+
   return (
     <>
-      <SideDrawer>
+      {drawrIsOpen && <Backdrop onClick={drawerHandler} />}
+
+      <SideDrawer show={drawrIsOpen} onClick={drawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={drawerHandler}>
           <span />
           <span />
           <span />
