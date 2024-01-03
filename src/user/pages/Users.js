27266d1d4a142755447e84d33835ee0b2f4f315f
dnjstd <dnjstd@gmail.com>
2024-01-03 22:49:38 +0900

    Feat: PC/Mobile nav

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <div>유저</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "SoyeonWon",
+      image:
+        "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
