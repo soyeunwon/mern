28a2bcd369d9be3dfac00952cacfe69063eea11b
dnjstd <dnjstd@gmail.com>
2024-01-03 16:55:17 +0900

    Feat: 라우트 설정

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h2>place</h2>;
+};
+
+export default NewPlace;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = () => {
+  return <div>유저</div>;
+};
+
+export default Users;
