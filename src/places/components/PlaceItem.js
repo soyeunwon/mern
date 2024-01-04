d73a05da9c50f54926b8dcb5c7e34c1086818138
dnjstd <dnjstd@gmail.com>
2024-01-04 23:30:09 +0900

    Feat: Modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlaceItem.css";
 import Card from "../../shared/components/UIElement/Card";
 import Button from "../../shared/components/Form/Button";
+import Modal from "../../shared/components/UIElement/Modal";
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const showMapHandler = () => {
+    setShowMap(!showMap);
+  };
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={showMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={showMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>지도</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>지도보기</Button>
-          <Button to={`/places/${props.id}`}>수정</Button>
-          <Button danger>삭제</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={showMapHandler}>
+              지도보기
+            </Button>
+            <Button to={`/places/${props.id}`}>수정</Button>
+            <Button danger>삭제</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
