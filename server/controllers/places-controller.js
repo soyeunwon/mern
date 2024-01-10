fa821469367bab6253cdc3234964c73691ecb8d0
dnjstd <dnjstd@gmail.com>
2024-01-11 01:37:08 +0900

    add places-controller

---
+++ server/controllers/places-controller.js
@@ -0,0 +1,51 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "서울 시청",
+    description: "설명입니다",
+    imageUrl:
+      "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
+    address: "서울 시청 주소~",
+    location: { lat: 37.5665, lng: 126.978 },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "용산구청",
+    description: "설명입니다",
+    imageUrl:
+      "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
+    address: "용산구청주소~",
+    location: { lat: 37.5311, lng: 126.9815 },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    throw new HttpError("해당 ID에 대한 장소를 찾지 못했습니다.", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((place) => place.creator === userId);
+
+  if (!place) {
+    return next(
+      new HttpError("해당 사용자ID에 대한 장소를 찾지 못했습니다.", 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,53 +1,11 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
+
+const placesControllers = require("../controllers/places-controller");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "서울 시청",
-    description: "설명입니다",
-    imageUrl:
-      "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
-    address: "서울 시청 주소~",
-    location: { lat: 37.5665, lng: 126.978 },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "용산구청",
-    description: "설명입니다",
-    imageUrl:
-      "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
-    address: "용산구청주소~",
-    location: { lat: 37.5311, lng: 126.9815 },
-    creator: "u2",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
-
-  if (!place) {
-    throw new HttpError("해당 ID에 대한 장소를 찾지 못했습니다.", 404);
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((place) => place.creator === userId);
-
-  if (!place) {
-    return next(
-      new HttpError("해당 사용자ID에 대한 장소를 찾지 못했습니다.", 404)
-    );
-  }
-
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
