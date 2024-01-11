47ed62da222e695efb708ba0b4e22cd7bda8e6e7
dnjstd <dnjstd@gmail.com>
2024-01-11 15:24:47 +0900

    add POST request and apply UUID v4

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use("/api/places", placesRoutes); // /api/places/...
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes);
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
