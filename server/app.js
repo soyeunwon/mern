dc1b688873abcc9b29622431aaa1822550fc0a2a
dnjstd <dnjstd@gmail.com>
2024-01-11 16:15:41 +0900

    add PATCH DELETE

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("라우트를 찾지 못했습니다.", 404);
+  throw error;
+}); // 처리하고자 하는 요청이 아닌 경우에 실행. 요청이 보내지지 않음. (잘못된 EndPoint 등).
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -2,7 +2,7 @@ const uuid = require("uuid");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "서울 시청",
@@ -36,17 +36,17 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((place) => place.creator === userId);
+  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-  if (!place) {
+  if (!places || !places.length) {
     return next(
       new HttpError("해당 사용자ID에 대한 장소를 찾지 못했습니다.", 404)
     );
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -66,6 +66,32 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = {
+    ...DUMMY_PLACES.find((p) => p.id === placeId),
+    title,
+    description,
+  };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
+  res.status(200).json({ message: "Deleted place", placeId: placeId });
+};
+
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
