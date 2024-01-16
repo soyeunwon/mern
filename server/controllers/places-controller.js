122f21b032da11be294949cb8d858b24fe3c6096
dnjstd <dnjstd@gmail.com>
2024-01-16 16:29:07 +0900

    express validator

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -50,6 +51,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("유효하지 않은 데이터입니다.", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
 
   const createdPlace = {
@@ -67,6 +73,11 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("유효하지 않은 데이터입니다.", 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -84,6 +95,10 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError("해당 장소id를 찾지 못했습니다.", 404);
+  }
 
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
 
--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,1 +1,51 @@
-const getPlaceById = () => {};
+const uuid = require("uuid");
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  { id: "u1", name: "soyeon", email: "test@test.com", password: "tester" },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("유효하지 않은 데이터입니다.", 422);
+  }
+
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) throw new HttpError("이미 존재하는 이메일입니다.", 422);
+
+  const createdUser = {
+    id: uuid.v4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("Unauthorized. 사용자를 식별할 수 없습니다.", 401);
+  }
+
+  res.json({ message: "Logged in!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
