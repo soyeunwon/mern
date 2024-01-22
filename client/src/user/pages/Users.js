4c4bafdfce4c432c74ccfaa69493de47d7f84cee
dnjstd <dnjstd@gmail.com>
2024-01-22 22:44:48 +0900

    로그인 요청, useHttpClient hook 추가

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElement/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "SoyeonWon",
-      image:
-        "https://img.freepik.com/free-vector/weather-icons-collection_1167-124.jpg?size=626&ext=jpg",
-      places: 3,
-    },
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState([]);
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5050/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) throw new Error(responseData.message);
+
+        setLoadedUsers(responseData.users);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      <UsersList items={loadedUsers} />
+    </>
+  );
 };
 
 export default Users;
