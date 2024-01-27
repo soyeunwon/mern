901a6c0682dfc0525dda3be0035b421dfdc4d7c6
dnjstd <dnjstd@gmail.com>
2024-01-27 23:15:17 +0900

    OPTIONS 요청 허용

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") return next();
+
   try {
     const token = req.headers.authorization.split(" ")[1]; // 'Bearer TOKEN'
     if (!token) throw new Error("인증 실패!");
